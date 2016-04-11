2f7709851ce819353289715cd19d1c965fe2f2e2
Ari Suarkadipa <suarkadipa@gmail.com>
2016-04-11 16:53:49 +0800

    updated
    remove settings bar in maps

--- app/src/main/java/qrcodescanner/agungmanuaba/com/qrcodescanner/activities/InfoMapsActivity.java
+++ app/src/main/java/qrcodescanner/agungmanuaba/com/qrcodescanner/activities/InfoMapsActivity.java
@@ -89,28 +89,6 @@ public class InfoMapsActivity extends AppCompatActivity implements GoogleApiClie
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onConnected(Bundle bundle) {
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation != null) {
